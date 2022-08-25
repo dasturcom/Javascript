486efa234c37f31548c0faf9e6fb8d3c9404af75
dasturcom <abdujabborshavkatjonov@gmail.com>
2022-08-25 22:57:11 +0500

    alert

--- js/index.js
+++ js/index.js
@@ -7,12 +7,12 @@ checkbox.addEventListener("click", function(){
 
 
 
-// var fristname = prompt("Ismingizni kiriting").toLocaleLowerCase().trim();
-// var surname = prompt("Familiyangizni kiriting").toLocaleLowerCase().trim();
-// var age = prompt("Yoshingizni kiriting").trim();
+var fristname = prompt("Ismingizni kiriting").toLocaleLowerCase().trim();
+var surname = prompt("Familiyangizni kiriting").toLocaleLowerCase().trim();
+var age = prompt("Yoshingizni kiriting").trim();
 
-// alert(`Sizning ismingiz ${fristname}, Familiyangiz ${surname}, Yoshingiz ${age}da`);
+alert(`Sizning ismingiz ${fristname}, Familiyangiz ${surname}, Yoshingiz ${age}da`);
 
-// console.log(`Ismingiz: ${fristname};`);
-// console.log(`Familiyangiz: ${surname};`);
-// console.log(`Yoshingiz: ${age}da;`); 
+console.log(`Ismingiz: ${fristname};`);
+console.log(`Familiyangiz: ${surname};`);
+console.log(`Yoshingiz: ${age}da;`); 
